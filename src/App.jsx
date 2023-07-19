cd4b9fd38909c98f3a2430dad3685d20354dc00a
Sabrina Drashen <sdrashen@gmail.com>
2023-07-19 15:24:48 -0300

    feat: create Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-function App() {
+import { Post } from './Post'
+import { Header } from './components/Header'
+import './styles.css'
 
+export function App() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Header/>
+
+      <Post 
+        author="Traveler Cat"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore pariatur voluptatibus, laboriosam nisi aliquid dignissimos iusto, et dolorem fugit praesentium libero ullam iste veniam sit quas incidunt. Nam, expedita inventore!"/>
+      <Post 
+        author="Cheff Cat"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore pariatur voluptatibus, laboriosam nisi aliquid dignissimos iusto, et dolorem fugit praesentium libero ullam iste veniam sit quas incidunt. Nam, expedita inventore!"/>
+    </div>
   )
 }
-
-export default App
