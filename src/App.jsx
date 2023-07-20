89440978b943acc71923db639a898a3e651d46c3
Sabrina Drashen <sdrashen@gmail.com>
2023-07-20 17:01:56 -0300

    style: App.module.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import { Post } from './Post'
 import { Header } from './components/Header'
+import styles from './App.module.css'
+
 import './global.css'
 
 export function App() {
   return (
     <div>
       <Header/>
-
-      <Post 
-        author="Traveler Cat"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore pariatur voluptatibus, laboriosam nisi aliquid dignissimos iusto, et dolorem fugit praesentium libero ullam iste veniam sit quas incidunt. Nam, expedita inventore!"/>
-      <Post 
-        author="Cheff Cat"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore pariatur voluptatibus, laboriosam nisi aliquid dignissimos iusto, et dolorem fugit praesentium libero ullam iste veniam sit quas incidunt. Nam, expedita inventore!"/>
+      <div className={styles.wrapper}>
+        <aside>
+          side bar
+        </aside>
+        <main>
+          <Post 
+          author="Traveler Cat"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore pariatur voluptatibus, laboriosam nisi aliquid dignissimos iusto, et dolorem fugit praesentium libero ullam iste veniam sit quas incidunt. Nam, expedita inventore!"/>
+          <Post 
+          author="Cheff Cat"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore pariatur voluptatibus, laboriosam nisi aliquid dignissimos iusto, et dolorem fugit praesentium libero ullam iste veniam sit quas incidunt. Nam, expedita inventore!"/>
+        </main>
+      </div>
+     
     </div>
   )
 }
