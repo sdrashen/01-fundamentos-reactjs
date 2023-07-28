66541bea6b66c0eed0ff5d4a568923ffed569ef3
Sabrina Drashen <sdrashen@gmail.com>
2023-07-28 15:31:18 -0300

    feat:create avatar compoent

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,6 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar'
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -10,7 +12,7 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50" />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/53306448?v=4" />
+        <Avatar src="https://github.com/diego3g.png" />
         <strong>Sabrina Drashen</strong>
         <span>Front End Developer</span>
       </div>
