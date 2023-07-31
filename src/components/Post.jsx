fce4967502a3d3957e72c407e3be0699cd4796d6
Sabrina Drashen <sdrashen@gmail.com>
2023-07-31 15:09:15 -0300

    feat: add useStaet to Comments

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,8 +5,14 @@ import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 
 import styles from './Post.module.css'
+import { useState } from 'react'
 
 export function Post({author, publishedAt, content}) {
+
+  const [comments, setComments] = useState([
+    1,
+    2,
+  ])
 
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR,
@@ -17,6 +23,11 @@ export function Post({author, publishedAt, content}) {
     addSuffix: true,
   })
   /**const Publicado a tanto tempo da data atual */
+
+  function handleCreateNewComment() {
+    event.preventDefault()
+    setComments([...comments, comments.length + 1]);
+  }
   return (
      <article className={styles.post}>
       <header>
@@ -43,7 +54,7 @@ export function Post({author, publishedAt, content}) {
         })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>
           Deixe seu feedback
         </strong>
@@ -54,9 +65,11 @@ export function Post({author, publishedAt, content}) {
           </footer>
       </form>
       <div className={styles.commentList}>
-        <Comment/>
-        <Comment/>
-        <Comment/>
+        {comments.map(comment => {
+          return (
+            <Comment />
+          )
+        })}
       </div>
      </article> 
   )
